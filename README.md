5633003884506b6affac48cf839538da8004ca52
TaurusCommit91 <105094991+TaurusCommit91@users.noreply.github.com>
2022-07-16 14:47:25 -0400

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+# MonopolyAR
+
+Developed with Unreal Engine 5
---
+++ Source/MonopolyAR/CharacterMovementComponent2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CharacterMovementComponent2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONOPOLYAR_API UCharacterMovementComponent2 : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
